23ca254708271ed0a4954a9a7e5b10750059eb34
Kwasi <rasbarbies@yahoo.com>
2022-11-19 01:06:28 +0000

    security groups

---
+++ sec-gp.tf
@@ -0,0 +1,121 @@
+# Creating Security Group
+resource "aws_security_group" "vprofile-bean-elb-sg" {
+  name        = "vprofile-bean-elb-sg"
+  description = "security group for bean-elb"
+  vpc_id      = aws_vpc.vprofile-vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+
+resource "aws_security_group" "vprofile-bastion-sg" {
+  name        = "vprofile-bastion-sg"
+  description = "security group for bastion ec2 instance"
+  vpc_id      = aws_vpc.vprofile-vpc.id
+
+  ingress {
+
+    from_port   = 0
+    to_port     = 0
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+}
+
+resource "aws_security_group" "vprofile-prod-sg" {
+  name        = "vprofile-prod-sg"
+  description = "security group for beanstalk instances"
+  vpc_id      = aws_vpc.vprofile-vpc.id
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    security_groups = [aws_security_group.vprofile-bastion-sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+resource "aws_security_group" "vprofile-backend-sg" {
+  name        = "vprofile-backend-sg"
+  description = "security group for RDS, active mq, elastic cache"
+  vpc_id      = aws_vpc.vprofile-vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    security_groups  = [aws_security_group.vprofile-prod-sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+resource "aws_security_group_rule" "sec_group_allow_itself" {
+  type                     = "ingress"
+  from_port                = 0
+  to_port                  = 65535
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.vprofile-backend-sg.id
+  source_security_group_id = aws_security_group.vprofile-backend-sg.id
+}
+
+#### copied up to this point
+
+
+resource "aws_security_group" "sec-gp" {
+  name        = "allow http"
+  description = "allow http for inbound traffic"
+  vpc_id      = aws_vpc.vprofile-vpc.id
+
+  ingress {
+    description = "http from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+
+
+  # HTTPS access from anywhere
+
+
+# Create Database Security Group
